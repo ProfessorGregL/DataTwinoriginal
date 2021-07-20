3445730d272c8bd55448817e62f00167030ebee7
Greg <5199464+stumpygreg@users.noreply.github.com>
2021-07-19 21:40:57 -0500

    Listen to 3000

--- client/server.js
+++ client/server.js
@@ -17,4 +17,5 @@ app.get("*", function(req, res){
   res.sendFile(__dirname + "/build/index.html");
 });
 // Listen to port 3000
-app.listen(process.env.PORT || 3000);
+//app.listen(process.env.PORT || 3000);
+app.listen(3000);
